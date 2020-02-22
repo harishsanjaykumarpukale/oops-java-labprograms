693026ed7c529c0103a78d62094befce205a668e
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2020-02-22 19:59:53 +0530

    Update ThirdSem.java

--- ThirdSem.java
+++ ThirdSem.java
@@ -1,5 +1,4 @@
-package com.company.CSE;
-import java.util.*;
+package CSE;
 
 public class ThirdSem  {
     void methodDefault(){
@@ -17,14 +16,66 @@ public class ThirdSem  {
 
     public static void main(String[] args){
         System.out.println("Section:Same Class");
+
         ThirdSem thirdSem = new ThirdSem();
-        thirdSem.AccessSpecifiersChecking();
+
+        thirdSem.methodDefault();
+        thirdSem.methodPrivate();
+        thirdSem.methodProtected();
+        thirdSem.WelcomeMsg();
+    }
+}
+
+class IseDept extends ThirdSem{
+    public static void main(String[] args){
+        System.out.println("Section:Same Package Sub-Class");
+        IseDept iseDept = new IseDept();
+
+        iseDept.AccessSpecifiersChecking();
     }
 
+
     public void AccessSpecifiersChecking(){
+        super.methodDefault();
         this.methodDefault();
-        this.methodPrivate();
+//        super.methodPrivate();
+        super.methodProtected();
         this.methodProtected();
+        super.WelcomeMsg();
         this.WelcomeMsg();
     }
+
+
+    @Override
+    void methodDefault(){
+        System.out.println("Inside Overridden Default method.");
+    }
+
+
+    @Override
+    protected void methodProtected(){
+        System.out.println("Inside Overridden protected method.");
+    }
+
+
+    @Override
+    public void WelcomeMsg(){
+
+        System.out.println("Inside Overridden public method.");
+    }
+
+
+
+}
+
+class EceDept {
+    public static void main(String[] args){
+        System.out.println("Section:Same Package Non-Sub-Class");
+        ThirdSem thirdSem = new ThirdSem();
+        thirdSem.methodDefault();
+//        thirdSem.methodPrivate();
+        thirdSem.methodProtected();
+        thirdSem.WelcomeMsg();
+    }
+
 }
