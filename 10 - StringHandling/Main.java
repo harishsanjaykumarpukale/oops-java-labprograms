409fdc35677ad398efeafdad56e41693dfc30e85
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2020-05-08 16:05:45 +0530

    Create Main.java

---
+++ 10 - StringHandling/Main.java
@@ -0,0 +1,16 @@
+package StringHandling;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		String sentence;
+		int choice;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("\n Press \n 1 for penulutimate word \n 2 for replace \n 3 for substrings \n:");
+		choice = Integer.parseInt(scanner.nextLine());
+		switch(choice) {
+		case 1:
+		}
+	}
+}
